2fb353091fe2f26567211bb082a81479e13bb41f
Hamish <hamishapps@gmail.com>
2024-06-06 17:22:29 +1200

    smal changes

--- src/components/Window.js
+++ src/components/Window.js
@@ -1,14 +1,13 @@
 import * as THREE from 'three';
 
-export function createWindow(scene) {
+export function createWindow(scene, position = { x: 0, y: 0, z: -3 }) {
   const geometry = new THREE.PlaneGeometry(1, 1);
   const material = new THREE.MeshBasicMaterial({ color: 0xffffff, side: THREE.DoubleSide });
   const plane = new THREE.Mesh(geometry, material);
 
-  // Position the plane
-  plane.position.set(0, 0, -3);
-
-  // Add plane to the scene
+  plane.position.set(position.x, position.y, position.z);
   scene.add(plane);
+
+  return plane;
 }
 
